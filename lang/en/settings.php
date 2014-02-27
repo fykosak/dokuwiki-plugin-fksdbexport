ca8433cb62dc056a44704357ff6ceeec44ed2f5d
Michal Koutný <xm.koutny@gmail.com>
2014-02-27 16:20:20 +0100

    added localization

---
+++ lang/en/settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * english language file for fksdbexport plugin
+ *
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+
+// keys need to match the config setting name
+$lang['url'] = 'Web service URL';
+//$lang['user'] = 'User';
+//$lang['password'] = 'Password';
+$lang['expiration'] = 'Cache expiration in seconds';
+
+
+
+//Setup VIM: ex: et ts=4 :
