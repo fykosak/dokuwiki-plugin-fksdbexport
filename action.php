bf2fceb0a89dc0c1c6356af4b1e2006a6760444b
Michal "mišo" Červeňák <miso@fykos.cz>
2020-08-30 12:51:04 +0200

    Fix php74 compatibility

--- action.php
+++ action.php
@@ -1,37 +1,37 @@
 <?php
 
+use dokuwiki\Extension\ActionPlugin;
+use dokuwiki\Extension\Event;
+use dokuwiki\Extension\EventHandler;
+
 /**
  * DokuWiki Plugin fksdbexport (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Michal Koutný <michal@fykos.cz>
  */
-// must be run within Dokuwiki
-if (!defined('DOKU_INC'))
-    die();
-
-class action_plugin_fksdbexport extends DokuWiki_Action_Plugin {
+class action_plugin_fksdbexport extends ActionPlugin {
 
     /**
      * Registers a callback function for a given event
      *
-     * @param Doku_Event_Handler $controller DokuWiki's event controller object
+     * @param EventHandler $controller DokuWiki's event controller object
      * @return void
      */
-    public function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('PARSER_CACHE_USE', 'BEFORE', $this, 'handle_parser_cache_use');
+    public function register(EventHandler $controller): void {
+        $controller->register_hook('PARSER_CACHE_USE', 'BEFORE', $this, 'handleParserCacheUse');
     }
 
     /**
      * [Custom event handler which performs action]
      *
-     * @param Doku_Event $event  event object by reference
-     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     * @param Event $event event object by reference
+     * @param mixed $param [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      * @return void
      */
-    public function handle_parser_cache_use(Doku_Event &$event, $param) {
-        $cache = & $event->data;
+    public function handleParserCacheUse(Event $event, $param): void {
+        $cache = &$event->data;
 
         // we're only interested in wiki pages
         if (!isset($cache->page)) {
@@ -48,7 +48,4 @@ class action_plugin_fksdbexport extends DokuWiki_Action_Plugin {
         }
         $cache->depends['files'] = !empty($cache->depends['files']) ? array_merge($cache->depends['files'], $depends) : $depends;
     }
-
 }
-
-// vim:ts=4:sw=4:et:
